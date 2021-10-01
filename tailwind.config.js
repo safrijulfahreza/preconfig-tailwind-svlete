0349b171f0c2ed14772af710008a82fc3182289e
rezadyk <safri.110798@gmail.com>
2021-10-01 17:09:25 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: {
+    content: ["./public/index.html", "./src/**/*.svelte"],
+    enabled: "production", // disable purge in dev
+    options: {
+      defaultExtractor: (content) => [
+        ...(content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []),
+        ...(content.match(/(?<=class:)[^=>\/\s]*/g) || []),
+      ],
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+  },
+};
